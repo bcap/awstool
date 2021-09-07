d6f341c8b4fff043441cd041877d77c2ac8d6d90
Bruno Penteado <polaco@gmail.com>
2021-09-07 12:58:49 +0100

    improving release script

--- release.sh
+++ release.sh
@@ -16,14 +16,13 @@ if [[ $(git status -s | wc -l) -gt 0 ]]; then
     exit 1
 fi
 
+log "building and pushing docker image"
+IMG=bcap/awstool:latest
+docker build -t $IMG . && docker push $IMG
+
 log "pushing changes to github"
 git push 
 
 log "building and creating github release"
 (cd cmd/awstool && go build -v -o $TMPDIR/awstool)
 gh release create $(date +%Y%m%d-%H%M) $TMPDIR/awstool < /dev/null
-
-log "building and pushing docker image"
-IMG=bcap/awstool:latest
-docker build -t $IMG . && docker push $IMG
-
