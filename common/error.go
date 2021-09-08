19fe0ada7385a1148e6c40f38e0e66cbda57a7f4
Bruno Penteado <polaco@gmail.com>
2021-09-08 14:08:02 +0100

    improving behaviour of Errors struct used to hold multiple errors

--- common/error.go
+++ common/error.go
@@ -14,9 +14,15 @@ func NewErrors(errors []error) Errors {
 }
 
 func (e Errors) Error() string {
+	if len(e.Errors) == 0 {
+		return ""
+	}
+	if len(e.Errors) == 1 {
+		return e.Errors[0].Error()
+	}
 	errorStrings := make([]string, len(e.Errors))
 	for i, err := range e.Errors {
-		errorStrings[i] = err.Error()
+		errorStrings[i] = "\"" + err.Error() + "\""
 	}
-	return fmt.Sprintf("multiple errors: [%s]", strings.Join(errorStrings, ","))
+	return fmt.Sprintf("multiple errors: [%s]", strings.Join(errorStrings, ", "))
 }
